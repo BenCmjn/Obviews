f06b6d072161fa7fde7fdd2abda37911bda9c05b
BenCmjn <benjamin.calmejane@gmail.com>
2018-03-13 16:13:38 +0100

    board controller created, index rooted

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  root 'home#index'
-  get 'home/index'
+  root 'board#index'
+  get 'board/index'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
