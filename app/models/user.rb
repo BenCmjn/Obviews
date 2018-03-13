1bd05c5199324363e5918223b509cb26dc9bd084
Antonin <antonin.siguierfr@gmail.com>
2018-03-13 18:45:33 +0100

    added some rspec tests

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,10 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
+  validates :email, presence: true, format: { with: VALID_EMAIL_REGEX }
+
+  validates :firstname, presence: true, length: { in: 2..50 }
+
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,11 +3,22 @@ require 'rails_helper'
 RSpec.describe User, type: :model do
   
   it "User is a class" do
-    expect(User).to be_a(Class)
+    expect(User).to be_a(Class) 
   end
 
   it "Creates a User instance and saves it" do
     user1 = User.create(email:"a@a.a", password:"coucou", password_confirmation:"coucou")
     expect(user1).to be_an_instance_of(User)
   end
+
+  it "Accepts a valid email" do
+    user1 = User.create(email:"r@r.r", password:"coucou", password_confirmation:"coucou")
+    expect(user1).to be_an_instance_of(User)
+  end
+  
+  it "Rejects an invalid email" do
+    user2 = User.create(email:"salut", password:"coucou", password_confirmation:"coucou")
+    expect(user2.valid?).to be false
+  end
+
 end
