7b16c863ad3b6861612dd8340d8c955b7e0f190c
Antonin <antonin.siguierfr@gmail.com>
2018-03-14 14:09:14 +0100

    created migration to add fields to board

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180314124311) do
+ActiveRecord::Schema.define(version: 20180314130717) do
 
   create_table "archived_boards", force: :cascade do |t|
     t.integer "user_id"
@@ -36,6 +36,14 @@ ActiveRecord::Schema.define(version: 20180314124311) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "user_id"
+    t.text "candidate_1_picture"
+    t.string "candidate_1_firstname"
+    t.string "candidate_1_lastname"
+    t.text "candidate_1_bio"
+    t.text "candidate_2_picture"
+    t.string "candidate_2_firstname"
+    t.string "candidate_2_lastname"
+    t.text "candidate_2_bio"
   end
 
   create_table "users", force: :cascade do |t|
