227e9d91e492351aad75c515b0343700072bf1c1
BenCmjn <benjamin.calmejane@gmail.com>
2018-03-14 19:48:42 +0100

    first test to have random Username in view

--- app/controllers/board_controller.rb
+++ app/controllers/board_controller.rb
@@ -1,12 +1,21 @@
 class BoardController < ApplicationController
   def get_ids
-  	ids = User.pluck(:id)       # User.pluck(:champ) va chercher toutes les instances de :champ dans la table User, puis le transforme en array
-  	@fate = ids.shuffle[0..1]   # On met ca dans une variable, puis on shuffle cette variable (on sort 2 valeurs au hasard de l'array)
-  	return @fate 
+  	
+  	# return @fate 
   end
 
-  def new
-  	candidate_1_id = @fate[0]   # @fate = 2 valeurs aléatoires que l'o va utiliser en tant que id pour le candidate 1 et 2
-  	candidate_2_id = @fate[1]
+  def index
+  	ids = User.pluck(:id)       # User.pluck(:champ) va chercher toutes les instances de :champ dans la table User, puis le transforme en array
+  	@fate = ids.shuffle[0..1]   # On met ca dans une variable, puis on shuffle cette variable (on sort 2 valeurs au hasard de l'array)
+
+  	@candidate_1_id = @fate[0]   # @fate = 2 valeurs aléatoires que l'o va utiliser en tant que id pour le candidate 1 et 2
+  	@candidate_2_id = @fate[1]
   end
+
+  def share
+  end
+
+  def confirmation
+  end
+
 end
