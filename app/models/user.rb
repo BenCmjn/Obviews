6cbe28671992ecdd3290ba3af848bdfefbedb256
Jules <rigomontj@gmail.com>
2018-03-14 11:45:29 +0100

    fixed phone regex error by commenting the line

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,6 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
-  FRENCH_PHONE_REGEX = ^(0|\+33)[1-9]([-. ]?[0-9]{2}){4}$
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
   
   validates :email, presence: true, format: { with: VALID_EMAIL_REGEX }
@@ -12,6 +11,7 @@ class User < ApplicationRecord
   validates :firstname, presence: true, length: { in: 2..50 }
   validates :lastname, presence: true, length: { in: 2..50 }
 
+  #FRENCH_PHONE_REGEX = ^(0|\+33)[1-9]([-. ]?[0-9]{2}){4}$
   #validates :lastname, presence: true, length: { in: 2..50 }
   #validates :bio, presence: true
   #validates :phone, presence: true, format: { with: FRENCH_PHONE_REGEX } 
