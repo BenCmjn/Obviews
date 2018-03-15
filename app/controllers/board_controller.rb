ce54ffa7a91b0ba9ee9ef3f3bbdff1a2674a6d39
Antonin <antonin.siguierfr@gmail.com>
2018-03-15 13:18:48 +0100

    deleted every call to database to check if heroku is OK

--- app/controllers/board_controller.rb
+++ app/controllers/board_controller.rb
@@ -5,11 +5,11 @@ class BoardController < ApplicationController
   end
 
   def index
-  	ids = User.pluck(:id)       # User.pluck(:champ) va chercher toutes les instances de :champ dans la table User, puis le transforme en array
-  	@fate = ids.shuffle[0..1]   # On met ca dans une variable, puis on shuffle cette variable (on sort 2 valeurs au hasard de l'array)
+  	# ids = User.pluck(:id)       # User.pluck(:champ) va chercher toutes les instances de :champ dans la table User, puis le transforme en array
+  	# @fate = ids.shuffle[0..1]   # On met ca dans une variable, puis on shuffle cette variable (on sort 2 valeurs au hasard de l'array)
 
-  	@candidate_1_id = @fate[0]   # @fate = 2 valeurs aléatoires que l'o va utiliser en tant que id pour le candidate 1 et 2
-  	@candidate_2_id = @fate[1]
+  	# @candidate_1_id = @fate[0]   # @fate = 2 valeurs aléatoires que l'o va utiliser en tant que id pour le candidate 1 et 2
+  	# @candidate_2_id = @fate[1]
   end
 
   def share
