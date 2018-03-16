549813f538b823ac7f06ff3dc4d7c6da6f31b2fe
Jules <rigomontj@gmail.com>
2018-03-16 19:14:54 +0100

    algo tbworked on

--- app/controllers/board_controller.rb
+++ app/controllers/board_controller.rb
@@ -1,26 +1,28 @@
 class BoardController < ApplicationController
   before_action :authenticate_user!
-  before_action :confirmation, :results
+  before_action :reset, :randomizer
   
   def get_ids
+  end
+  
+  def randomizer
+      ids = User.pluck(:id) # User.pluck(:champ) va chercher toutes les instances de :champ dans la table User, puis le transforme en array
+      ids.delete_if {|id| id == current_user.id } # Ne jamais être soi-même dans les propositions…
+      @fate = ids.shuffle[0..1]   # On met ca dans une variable, puis on shuffle cette variable (on sort 2 valeurs au hasard de l'array)
+      return @fate
   end
 
   def index
   end
 
-  def results
-    def randomizer
-      ids = User.pluck(:id)       # User.pluck(:champ) va chercher toutes les instances de :champ dans la table User, puis le transforme en array
-      ids.delete_if {|id| id == current_user.id } # Ne jamais être soi-même dans les propositions…
-      @fate = ids.shuffle[0..1]   # On met ca dans une variable, puis on shuffle cette variable (on sort 2 valeurs au hasard de l'array)
-      return @fate
-    end # Return two random differents user ids, without the current_user one.
+  def reset
     @lock = 0 # TO DELETE WHEN NEEDED!!!!
-    @result = randomizer
+    result = randomizer
+    swapper = result
     @board = Board.find(current_user.board_ids.first)
 
-    @candidate_1 = User.find(@result[0])
-    @candidate_2 = User.find(@result[1])
+    @candidate_1 = User.find(swapper[0])
+    @candidate_2 = User.find(swapper[1])
   end
 
   def show
@@ -32,11 +34,12 @@ class BoardController < ApplicationController
     #archivedboard.lock = @lock
     archivedboard.is_match = false
     archivedboard.save
+    @should_i_reset = true
     redirect_to board_show_path
   end
 
   def confirmation
-    #@intro = params[:intro] #this is the text you enter in the field thats a justification of the match
+     #@intro = params[:intro] #this is the text you enter in the field thats a justification of the match
   end
 
   def match
@@ -52,6 +55,7 @@ class BoardController < ApplicationController
     archivedboard.is_match = true
     #archivedboard.intro = @intro
     archivedboard.save
+    @should_i_reset = true
     redirect_to board_show_path
   end  
 
