8dde5ed8bbf484fee079739ab27bb0f2d9d5ab4c
Jules <rigomontj@gmail.com>
2018-03-16 12:39:37 +0100

    changed one line in board controller

--- app/controllers/board_controller.rb
+++ app/controllers/board_controller.rb
@@ -15,8 +15,8 @@ class BoardController < ApplicationController
       @fate = ids.shuffle[0..1]   # On met ca dans une variable, puis on shuffle cette variable (on sort 2 valeurs au hasard de l'array)
       return @fate
     end # Return two random differents user ids, without the current_user one.
-    @board = Board.where(user_id: current_user.id)
     @result = randomizer
+    @board = Board.find(current_user.board_ids.first)
 
     @candidate_1 = User.find(@result[0])
     @candidate_2 = User.find(@result[1])
