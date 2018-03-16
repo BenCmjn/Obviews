2edfc7333c7664106d4d51b99fe1f7a3b77e8ff4
Jules <rigomontj@gmail.com>
2018-03-16 16:45:32 +0100

    next added ! we're goooood

--- app/controllers/board_controller.rb
+++ app/controllers/board_controller.rb
@@ -15,6 +15,7 @@ class BoardController < ApplicationController
       @fate = ids.shuffle[0..1]   # On met ca dans une variable, puis on shuffle cette variable (on sort 2 valeurs au hasard de l'array)
       return @fate
     end # Return two random differents user ids, without the current_user one.
+    @lock = 0 # TO DELETE WHEN NEEDED!!!!
     @result = randomizer
     @board = Board.find(current_user.board_ids.first)
 
@@ -23,6 +24,10 @@ class BoardController < ApplicationController
   end
 
   def next
+    aboard = ArchivedBoard.create(user_id: current_user.id)
+    aboard.users=[@candidate_1, @candidate_2]
+    aboard.lock = @lock
+    aboard.save
     byebug
   end
 
