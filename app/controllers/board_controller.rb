00503b4a4b807d8ceea74f8fa556a23dbea008fe
Jules <rigomontj@gmail.com>
2018-03-19 12:47:55 +0100

    Comme neuf l'algo

--- app/controllers/board_controller.rb
+++ app/controllers/board_controller.rb
@@ -9,12 +9,18 @@ class BoardController < ApplicationController
   end
 
   def show
+    if current_user.first_time == true
+      cu = current_user
+      cu.first_time = false
+      cu.save
+      redirect_to randomizers_reset_path
+    end
   end
 
   def get_candidate_ids
-    @candidate_ids = File.read("tmp_current_candidates_ids.txt").split(",")
-    @candidate_1 = @candidate_ids[0]
-    @candidate_1 = @candidate_ids[1]
+    @candidate_1 = User.find(File.read("tmp_current_candidates_ids.txt").split(',')[0][1..-1])
+    @candidate_2 = User.find(File.read("tmp_current_candidates_ids.txt").split(',')[1][1..-2])
+    @candidate_ids = [@candidate_1, @candidate_2]
   end
 
   def next
@@ -23,7 +29,7 @@ class BoardController < ApplicationController
     #archivedboard.lock = @lock
     archivedboard.is_match = false
     archivedboard.save
-    redirect_to randomizers_reset
+    redirect_to randomizers_reset_path
     # redirect_to board_show_path
   end
 
@@ -44,7 +50,7 @@ class BoardController < ApplicationController
     archivedboard.is_match = true
     #archivedboard.intro = @intro
     archivedboard.save
-    redirect_to randomizers_reset
+    redirect_to randomizers_reset_path
   end  
 
   def share
