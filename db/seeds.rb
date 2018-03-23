a06a29bca9c5dc9b3ced2834a1105f38be4af3ce
Jules <rigomontj@gmail.com>
2018-03-23 19:14:37 +0100

    added clean seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,28 +1,71 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+user1 = User.new(bio: "La vie c’est avant tout des rencontres. Des gens qui m’ont tendu la main, peut-être à un moment où je ne pouvais pas, où j’étais seul chez moi. Et c’est assez curieux de se dire que les hasards, les rencontres forgent une destinée... Parce que quand on a le goût de la chose, quand on a le goût de la chose bien faite, le beau geste, parfois on ne trouve pas l’interlocuteur en face je dirais, le miroir qui vous aide à avancer. Alors ça n’est pas mon cas, comme je disais là, puisque moi au contraire, j’ai pu : et je dis merci à la vie, je lui dis merci, je chante la vie, je danse la vie... je ne suis qu’amour ! Et finalement, quand beaucoup de gens aujourd’hui me disent « Mais comment fais-tu pour avoir cette humanité ? », et bien je leur réponds très simplement, je leur dis que c’est ce goût de l’amour ce goût donc qui m’a poussé aujourd’hui à entreprendre une construction mécanique, mais demain qui sait ? Peut-être simplement à me mettre au service de la communauté, à faire le don, le don de soi...",
+                 firstname:"Otis",
+                 lastname:"Scribe",
+                 email:"otisscribe@gmail.com",
+                 password:"coucou", 
+                 password_confirmation:"coucou")
+                 user1.save
 
-user1 = User.new(bio: "Mais cette feature c'est 5min, montre en main hein, t'inquiètes...", firstname:"Jules", lastname:"Rigomont", email:"julesrigomont@gmail.com", password:"coucou", password_confirmation:"coucou")
-user1.save
+user1 = User.new(bio: "À l'occasion, je vous mettrai un petit coup de polish...",
+                 firstname:"Hubert", 
+                 lastname:"Bonisseur de La Bath", 
+                 email:"hubertbonisseurdelabath@gmail.com", 
+                 password:"coucou", 
+                 password_confirmation:"coucou")
+                 user1.save
 
-user2= User.new(bio: "AAAAAAAAAAAAAAAAAAAAAAAAAAAAhhhh!!!!!", firstname:"Benjamin", lastname:"Calméjane", email:"benjamincalmejane@gmail.com", password:"coucou", password_confirmation:"coucou")
-user2.save
+user1 = User.new(bio: "Je crois que toi et moi, on a un peu le même problème, c'est à dire qu'on peut pas vraiment tout miser sur notre physique, surtout toi.",
+                 firstname:"Jean-Claude", 
+                 lastname:"Dusse", 
+                 email:"jeanclaudedusse@gmail.com", 
+                 password:"coucou",
+                 password_confirmation:"coucou")
+                 user1.save
 
-user3 = User.new(bio: "T'es le meilleur.", firstname:"Charles", lastname:"Dacquay", email:"charlesdacquay@gmail.com", password:"coucou", password_confirmation:"coucou")
-user3.save
+user1 = User.new(bio: "Mais cette feature c'est 5min montre en main hein, t'inquiètes...", 
+                 firstname:"Jules", 
+                 lastname:"Rigomont", 
+                 email:"rigomontj@gmail.com", 
+                 password:"coucou", 
+                 password_confirmation:"coucou")
+                 user1.save
 
-user4 = User.new(bio: "Alors sinon on peut faire des stickers THP...", firstname:"Félix", lastname:"Gaudé", email:"felixgaude@gmail.com", password:"coucou", password_confirmation:"coucou")
-user4.save
+user1 = User.new(bio: "Obviews? C'est avant tout du plaisir, et beaucoup de sueur.",
+                 firstname:"Benjamin", 
+                 lastname:"Calméjane", 
+                 email:"benjamin.calmejane@gmail.com", 
+                 password:"coucou", 
+                 password_confirmation:"coucou")
+                 user1.save
 
-user5 = User.new(bio: "Salut! Tu vas au CIDJ vendredi?", firstname:"Ségolène", lastname:"Alquier", email:"segolenealquier@gmail.com", password:"coucou", password_confirmation:"coucou")
-user5.save
+user1 = User.new(bio: "T'es le meilleur.", 
+                 firstname:"Charles", 
+                 lastname:"Dacquay", 
+                 email:"charlesdacquay@gmail.com", 
+                 password:"coucou", 
+                 password_confirmation:"coucou")
+                 user1.save
 
-user6 = User.new(bio: "Algo floco is my credo.", firstname:"Antonin", lastname:"Siguier", email:"antoninsiguier@gmail.com", password:"coucou", password_confirmation:"coucou")
-user6.save
+user1 = User.new(bio: "Alors sinon on peut faire des stickers THP...", 
+                 firstname:"Félix",
+                 lastname:"Gaudé", 
+                 email:"felixgaude@gmail.com", 
+                 password:"coucou", 
+                 password_confirmation:"coucou")
+                 user1.save
 
-user7 = User.new(bio: "Mailjet c'est vraiment génial!!!", firstname:"Thomas", lastname:"Darrigade", email:"thomasdarrigade@gmail.com", password:"coucou", password_confirmation:"coucou")
-user7.save
+user1 = User.new(bio: "Salut! Tu vas au CIDJ vendredi?", 
+                 firstname:"Ségolène", 
+                 lastname:"Alquier", 
+                 email:"segolene.alquier@gmail.com", 
+                 password:"coucou", 
+                 password_confirmation:"coucou")
+                 user1.save
+
+user1 = User.new(bio: "Algo floco is my credo.", 
+                 firstname:"Antonin", 
+                 lastname:"Siguier", 
+                 email:"antonin.siguierfr@gmail.com",
+                 password:"coucou",
+                 password_confirmation:"coucou")
+                 user1.save
